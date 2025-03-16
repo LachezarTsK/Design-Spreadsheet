e69ca58bf15d6fe56d6ccadde1915e6b7c4598a0
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-03-16 18:41:55 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,36 @@
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Spreadsheet {
+
+    static const int EMPTY_CELL = 0;
+    unordered_map<string, int> cellsToValues;
+
+public:
+    Spreadsheet(int rows) {}
+
+    void setCell(const string& cell, int value) {
+        cellsToValues[cell] = value;
+    }
+
+    void resetCell(const string& cell) {
+        cellsToValues.erase(cell);
+    }
+
+    int getValue(const string& formula) {
+        int indexSecondOperator = formula.find('+');
+        int firstValue = extractValue(formula.substr(1, indexSecondOperator - 1));
+        int secondValue = extractValue(formula.substr(indexSecondOperator + 1));
+        return firstValue + secondValue;
+    }
+
+private:
+    int extractValue(const string& operand) {
+        if (isalpha(operand[0])) {
+            return cellsToValues.contains(operand) ? cellsToValues.at(operand) : EMPTY_CELL;
+        }
+        return stoi(operand);
+    }
+};
