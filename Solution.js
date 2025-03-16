8f09aea17e9c4d638901a600797dc6bf6a41fbb4
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-03-16 18:10:50 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,53 @@
+
+class Spreadsheet {
+
+    static #EMPTY_CELL = 0;
+    static #ASCII_ZERO = 48;
+    #cellsToValues;
+
+    /**
+     * @param {number} rows
+     */
+    constructor(rows) {
+        this.#cellsToValues = new Map();
+    }
+
+    /** 
+     * @param {string} cell 
+     * @param {number} value
+     * @return {void}
+     */
+    setCell(cell, value) {
+        this.#cellsToValues.set(cell, value);
+    }
+
+    /** 
+     * @param {string} cell
+     * @return {void}
+     */
+    resetCell(cell) {
+        this.#cellsToValues.delete(cell);
+    }
+
+    /** 
+     * @param {string} formula
+     * @return {number}
+     */
+    getValue(formula) {
+        const indexSecondOperator = formula.indexOf('+');
+        const firstValue = this.#extractValue(formula.substring(1, indexSecondOperator));
+        const secondValue = this.#extractValue(formula.substring(indexSecondOperator + 1));
+        return firstValue + secondValue;
+    }
+
+    /** 
+     * @param {string} operand
+     * @return {number}
+     */
+    #extractValue(operand) {
+        if (/[A-Z]/.test(operand.charAt(0))) {
+            return this.#cellsToValues.has(operand) ? this.#cellsToValues.get(operand) : Spreadsheet.#EMPTY_CELL;
+        }
+        return Number(operand);
+    }
+}
